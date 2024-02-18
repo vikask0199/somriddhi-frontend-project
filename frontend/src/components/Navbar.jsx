1ff0eee81ffeeda95975fcc6bba3074bf6fd25c5
viku135790 <viku135790@gmail.com>
2024-02-18 12:49:09 +0530

    navigation fixed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -32,13 +32,13 @@ const Navbar = () => {
             About
           </div>
         </li>
-        <li className={`h-16 flex items-center cursor-pointer ${activeLink === 3 ? 'border-b-4 border-red-600 text-red-600' : ''}`}>
-          <div className='' onClick={() => setActiveLink(3)} to="/our-blogs">
+        <li className={`h-16 flex items-center cursor-pointer ${activeLink === 2 ? 'border-b-4 border-red-600 text-red-600' : ''}`}>
+          <div className='' onClick={() => setActiveLink(2)} to="/our-blogs">
             Blogs
           </div>
         </li>
-        <li className={`h-16 flex items-center cursor-pointer ${activeLink === 2 ? 'border-b-4 border-red-600 text-red-600' : ''}`}>
-          <div className='' onClick={() => setActiveLink(2)} to="/our-services">
+        <li className={`h-16 flex items-center cursor-pointer ${activeLink === 3 ? 'border-b-4 border-red-600 text-red-600' : ''}`}>
+          <div className='' onClick={() => setActiveLink(3)} to="/our-services">
             Services
           </div>
         </li>
@@ -52,27 +52,27 @@ const Navbar = () => {
 
       {/* mobile view */}
       <ul className={`md:hidden z-40 absolute bg-black h-full w-screen bottom-0 top-16 pt-5 pl-4 duration-500 ${open ? 'left-0' : 'left-[-100%]'}`}>
-        <li onClick={() => setOpen(!open)}>
+        <li onClick={() => {setOpen(!open), setActiveLink(0)}}>
           <div className={`px-0 my-5 py-3 inline-block w-full ${activeLink === 0 ? 'border-b-2 border-red-700 text-red-700' : ''}`} to="/" onClick={() => setActiveLink(1)}>
             Home
           </div>
         </li>
-        <li onClick={() => setOpen(!open)}>
+        <li onClick={() => {setOpen(!open), setActiveLink(1)}}>
           <div className={`px-0 my-5 py-3 inline-block w-full ${activeLink === 1 ? 'border-b-2 border-red-700 text-red-700' : ''}`} to="/about-us" onClick={() => setActiveLink(1)}>
             About
           </div>
         </li>
-        <li onClick={() => setOpen(!open)}>
+        <li onClick={() => {setOpen(!open), setActiveLink(2)}}>
           <div className={`px-0 my-5 py-3 inline-block w-full ${activeLink === 2 ? 'border-b-2 border-red-700 text-red-700' : ''}`} to="/our-blogs" onClick={() => setActiveLink(1)}>
             Blogs
           </div>
         </li>
-        <li onClick={() => setOpen(!open)}>
+        <li onClick={() => {setOpen(!open), setActiveLink(3)}}>
           <div className={`px-0 my-5 py-3 inline-block w-full ${activeLink === 3 ? 'border-b-2 border-red-700 text-red-700' : ''}`} to="/our-services" onClick={() => setActiveLink(1)}>
             Services
           </div>
         </li>
-        <li onClick={() => setOpen(!open)}>
+        <li onClick={() => {setOpen(!open), setActiveLink(4)}}>
           <div className={`px-0 my-5 py-3 inline-block w-full ${activeLink === 4 ? 'border-b-2 border-red-700 text-red-700' : ''}`} to="/contact-us" onClick={() => setActiveLink(1)}>
             Contact Us
           </div>
